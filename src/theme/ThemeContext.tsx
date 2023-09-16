914e3d478c4b0131e332d71f2b6172891c2777c0
Vladislav Ivanyuk <vl.ivanyuk1994@gmail.com>
2023-09-16 15:10:18 +1000

    [~] little refactor context

---
+++ src/theme/ThemeContext.tsx
@@ -0,0 +1,27 @@
+import { FC, createContext, useState } from "react";
+
+export const LOCAL_STORAGE_THEME_KEY = 'theme';
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark'
+}
+export interface IThemeContextProps {
+    theme?: Theme,
+    setTheme?: (theme: Theme) => void;
+}
+export const ThemeContext = createContext<IThemeContextProps>({});
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
+
+export const ThemeProvider: FC = ({children}) => {
+    const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+    return (
+        <ThemeContext.Provider value={{
+            theme: theme,
+            setTheme: setTheme
+        }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
