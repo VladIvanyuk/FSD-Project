d0fbc700e916eaa632d552159580ff86ec744362
VladIvanyuk <vl.ivanyuk1994@gmail.com>
2023-09-18 09:44:39 +1000

    [+] add classnames

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy";
 import { MainPageLazy } from "./pages/MainPage/MainPage.lazy";
 import { Suspense } from "react";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 
 export const App = () => {
 
   const {theme, toggleTheme} = useTheme();
-
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>SWITCH THEME</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,14 @@
+type TMods = Record<string, boolean| string>;
+
+export const classNames = (cls: string, mods:TMods, additional: string[]) => {
+    
+    return [
+        cls, 
+        Object.keys(mods).filter((key) => mods[key]), 
+        additional
+    ]
+    .flat()
+    .join(' ')
+    
+}
+
