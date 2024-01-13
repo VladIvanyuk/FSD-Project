e8d862b44ab94fbb0e9e94470c4003b22d67ca9f
Vladislav Ivanyuk <vl.ivanyuk1994@gmail.com>
2024-01-13 15:48:27 +1000

    [-] delete unusual vars

--- src/entity/Profile/ui/ProfileCard/ProfileCard.tsx
+++ src/entity/Profile/ui/ProfileCard/ProfileCard.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react'
-import { classNames } from 'helpers/classNames/classNames'
 import cls from './ProfileCard.module.scss'
 import { useTranslation } from 'react-i18next'
 import { Text } from 'shared/ui/Text/Text'
@@ -7,19 +6,14 @@ import { AppButton, ButtonTheme } from 'shared/ui'
 import { Input } from 'shared/ui/Input/Input'
 import { useSelector } from 'react-redux'
 import { getProfileData } from '../../model/selectors/getProfileData/getProfileData'
-import { getProfileError } from '../../model/selectors/getProfileError/getProfileError'
-import { getProfileIsLoading } from '../../model/selectors/getProfileIsLoading/getProfileIsLoading'
 
 interface IProfileCardProps {
   className?: string
 }
 
 export const ProfileCard: FC<IProfileCardProps> = (props) => {
-    const { className } = props;
     const { t } = useTranslation();
     const data = useSelector(getProfileData)
-    const error = useSelector(getProfileError)
-    const isLoading = useSelector(getProfileIsLoading)
     return (
         <div className={cls.ProfileCard}>
             <div className={cls.header}>
