ecfb970456d339d82411b6e40b80339ae8b66798
Vlad Ivanyuk <vl.ivanyuk1994@gmail.com>
2024-02-14 19:04:18 +1000

    [+] articles buttons view

--- src/features/ArticleViewSelector/ui/ArticleViewSelector.tsx
+++ src/features/ArticleViewSelector/ui/ArticleViewSelector.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { classNames } from 'helpers/classNames/classNames'
 import cls from './ArticleViewSelector.module.scss'
 import { AppButton, ButtonTheme } from 'shared/ui'
@@ -13,12 +13,19 @@ interface IArticleViewSelectorProps {
 
 export const ArticleViewSelector: FC<IArticleViewSelectorProps> = (props) => {
     const { className, onClickHandler } = props;
+    const [isActiveToggle, setIsActiveToggle] = useState(true);
+
+    const onClickToggler = (view: ArticleListView, flag: boolean) => {
+        onClickHandler(ArticleListView[view]);
+        setIsActiveToggle(flag)
+    }
+
     return (
         <div className={classNames(cls.articleViewSelector, {}, [className])}>
-            <AppButton theme={ButtonTheme.CLEAR} onClick={() => { onClickHandler(ArticleListView.LIST); }}>
+            <AppButton className={classNames(cls.listBtn, { [cls.isActive]: isActiveToggle }, [className])} theme={ButtonTheme.CLEAR} onClick={() => { onClickToggler(ArticleListView.LIST, true) }}>
                 <Icon Svg={ListIcon} />
             </AppButton>
-            <AppButton theme={ButtonTheme.CLEAR} onClick={() => { onClickHandler(ArticleListView.GRID); }}>
+            <AppButton className={classNames(cls.gridBtn, { [cls.isActive]: !isActiveToggle }, [className])} theme={ButtonTheme.CLEAR} onClick={() => { onClickToggler(ArticleListView.GRID, false) }}>
                 <Icon Svg={GridIcon} />
             </AppButton>
         </div>
